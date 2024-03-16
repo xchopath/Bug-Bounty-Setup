f42a4fcd9bf997aaf0321f09e9429f58ef672906
Novran Faathir <novran.faathir@gmail.com>
2024-03-17 05:58:39 +0700

    Create h1-scopes-enum.sh

---
+++ h1-scopes-enum.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPT="${1}"
+
+for TARGET in $(cat h1-scopes.txt | grep 'scope:true' | grep ' \*\.' | awk -F '|' '{print $2}' | sed 's/,/ /g')
+do
+	eval ${SCRIPT} "${TARGET}"
+done
