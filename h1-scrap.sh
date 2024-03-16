b8a0c0feca9cb5d7f94d95b9771b504eb7a5e579
Novran Faathir <novran.faathir@gmail.com>
2024-03-17 04:24:09 +0700

    Update h1-scrap.sh

--- h1-scrap.sh
+++ h1-scrap.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# HackerOne Target Scraper by @xchopath
-# Dependencies: apt intall jq curl tee -y
-# Run: bash h1-scrap.sh | tee -a h1-scopes.txt
 
 curl -s "https://hackerone.com/directory/programs" -o h1-scrap.init -c h1-scrap.cookie
 
@@ -14,7 +11,7 @@ function getScopes() {
 	do
 		GET_SCOPES=$(curl -s -k -X 'POST' -H "X-Csrf-Token: ${CSRF_TOKEN}" -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.97 Safari/537.36' -H 'Content-Type: application/json' -H 'Origin: https://hackerone.com' -H 'Referer: https://hackerone.com/directory/programs' -b h1-scrap.cookie --data "{\"operationName\":\"PolicySearchStructuredScopesQuery\",\"variables\":{\"handle\":\"${HANDLE_ID}\",\"searchString\":\"\",\"eligibleForSubmission\":null,\"eligibleForBounty\":null,\"asmTagIds\":[],\"assetTypes\":[],\"from\":${SCOPE_PAGE},\"size\":100,\"sort\":{\"field\":\"cvss_score\",\"direction\":\"DESC\"},\"product_area\":\"h1_assets\",\"product_feature\":\"policy_scopes\"},\"query\":\"query PolicySearchStructuredScopesQuery(\$handle: String!, \$searchString: String, \$eligibleForSubmission: Boolean, \$eligibleForBounty: Boolean, \$minSeverityScore: SeverityRatingEnum, \$asmTagIds: [Int], \$assetTypes: [StructuredScopeAssetTypeEnum!], \$from: Int, \$size: Int, \$sort: SortInput) {\n  team(handle: \$handle) {\n    id\n    structured_scopes_search(\n      search_string: \$searchString\n      eligible_for_submission: \$eligibleForSubmission\n      eligible_for_bounty: \$eligibleForBounty\n      min_severity_score: \$minSeverityScore\n      asm_tag_ids: \$asmTagIds\n      asset_types: \$assetTypes\n      from: \$from\n      size: \$size\n      sort: \$sort\n    ) {\n      nodes {\n        ... on StructuredScopeDocument {\n          id\n          ...PolicyScopeStructuredScopeDocument\n          __typename\n        }\n        __typename\n      }\n      pageInfo {\n        startCursor\n        hasPreviousPage\n        endCursor\n        hasNextPage\n        __typename\n      }\n      total_count\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment PolicyScopeStructuredScopeDocument on StructuredScopeDocument {\n  id\n  identifier\n  display_name\n  instruction\n  cvss_score\n  eligible_for_bounty\n  eligible_for_submission\n  asm_system_tags\n  created_at\n  updated_at\n  attachments {\n    id\n    file_name\n    file_size\n    content_type\n    expiring_url\n    __typename\n  }\n  __typename\n}\n\"}" "https://hackerone.com/graphql")
 		echo "${GET_SCOPES}" | jq -r '.data.team.structured_scopes_search.nodes[] | "\(.identifier) | \(.display_name) | scope:\(.eligible_for_submission) | bounty:\(.eligible_for_bounty)"' | sed "s/^/${HANDLE_ID} | /g"
-		if [[ $(echo "${GET_SCOPES}" | jq -r '.data.team.structured_scopes_search.pageInfo.hasNextPage') =~ 'true' ]]; then
+		if [[ $(echo "${GET_SCOPES}" | jq -r '.data.team.structured_scopes_search.pageInfo.hasNextPage') == 'true' ]]; then
 			SCOPE_PAGE=$(($SCOPE_PAGE+100))
 		else
 			break
