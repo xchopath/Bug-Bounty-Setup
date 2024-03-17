1fddb6a8411a768accd4a5408281aaab8fa1c92a
Novran Faathir <novran.faathir@gmail.com>
2024-03-17 16:54:44 +0700

    Update installer-tools.sh

--- installer-tools.sh
+++ installer-tools.sh
@@ -45,3 +45,8 @@ mv gowitness /usr/local/bin/
 curl -s "https://api.github.com/repos/projectdiscovery/httpx/releases/latest" | grep "httpx_.*_linux_amd64.zip" | cut -d : -f 2,3 | tr -d \" | wget -qi -
 unzip -o $(ls | grep httpx_ | grep zip$)
 mv httpx /usr/local/bin/
+
+curl -s "https://api.github.com/repos/chaitin/xray/releases/latest" | grep "xray_linux_amd64.zip" | cut -d : -f 2,3 | tr -d \" | wget -qi -
+unzip -o $(ls | grep xray_linux_amd64 | grep zip$)
+chmod +x xray_linux_amd64
+./xray_linux_amd64
